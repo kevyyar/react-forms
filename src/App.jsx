07cd92e8545e4f96be23cd05db4447df1738871c
Kevin Barreto <barretokevin@hotmail.com>
2023-03-11 13:41:05 -0600

    add external api and add crud functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null)
 
-  const deleteBookById = (id) => {
+  useEffect(() => {
+    fetchBooks();
+  }, [])
+
+
+  const fetchBooks = async () => {
+    setLoading(true)
+    try {
+      const res = await axios.get("http://localhost:3001/books");
+      console.log(res.data)
+      setBooks(res.data)
+    } catch (err) {
+      setError(err)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`)
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
@@ -13,10 +35,14 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const res = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle
+    })
+    const { data } = res;
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...data };
       }
 
       return book;
@@ -25,11 +51,19 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
-    const updatedBooks = [...books, { id: crypto.randomUUID(), title }];
+  const createBook = async (title) => {
+    const res = await axios.post("http://localhost:3001/books", {
+      title
+    })
+    const { data } = res;
+
+    const updatedBooks = [...books, data];
 
     setBooks(updatedBooks);
   };
+
+  if (loading) return <div>Loading</div>
+  if (error) return <div>Error</div>
 
   return (
     <div className="app">
